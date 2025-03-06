4fbdb5f63d39da8fc23fb74baced40bbf7eab011
Ajay <ajayjogdand374@gmail.com>
2025-03-06 19:33:33 +0530

    feat:branch longitude and  latitude

--- realty_reflex_hrms/realty_reflex_hrms/employee_checkin.py
+++ realty_reflex_hrms/realty_reflex_hrms/employee_checkin.py
@@ -2,10 +2,12 @@ import frappe
 from geopy.distance import geodesic
 
 def is_inside_geofence(self,method):
-
-    doc=frappe.get_doc("Custom Settings")
-    if doc.latitude and doc.longitude and doc.allow_checkin_in_radius:
-        """Check if a point (device) is inside a circular geofence"""
-        distance = geodesic((self.latitude, self.longitude), (doc.latitude, doc.longitude)).meters
-        if distance >= doc.allow_checkin_in_radius:
-            frappe.throw("You didn't have allowed to checkin")
+    if self.employee:
+        emp=frappe.get_doc("Employee",self.employee)
+        if emp.branch:
+            branch=frappe.get_doc("Branch",emp.branch)
+            if branch.custom_latitude and branch.custom_longitude and branch.custom_radius_allowed_for_checkin:
+                """Check if a point (device) is inside a circular geofence"""
+                distance = geodesic((self.latitude, self.longitude), (branch.custom_latitude, branch.custom_longitude)).meters
+                if distance >= branch.custom_radius_allowed_for_checkin:
+                    frappe.throw("You didn't have allowed to checkin")
